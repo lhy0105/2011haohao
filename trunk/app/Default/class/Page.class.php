2cd2b633f52c0205178eebe56d8a9ca8421fc90f
baochuanzhou@gmail.com <baochuanzhou@gmail.com@31dcd45e-55b7-f6bc-f86d-74df720b1674>
2012-09-09 06:27:00 +0000

    重构代码，按照规范写代码

--- trunk/app/Default/class/Page.class.php
+++ trunk/app/Default/class/Page.class.php
@@ -1,23 +1,17 @@
 <?php
 class Default_Page extends Controller{
-	private $_username = 'test';
-	private $_password = '123123';
-
-	public function index(){
-	}
-
 	/**
-	 * 1:用户验证成功;2:用户验证失败;3:是第4次验证(这时候验证码验证);6:封锁IP;7:验证码不正确;
+	 * 1:Success;2:Failure;3:Forth validation(Show verified Code);6:No IP;7:The verified Code  Is Error!;
 	 */
 	public function login(){
 		$ip = ip2long(get_client_ip());
 		$user = Default_Model_User::getInstance();
-		$valid = $user->validClientIp($ip);
-		$timesLogin = $user->getLoginTimes($ip);
+		$valid = $user->validClientIP($ip);
+		$times_login =  $user->getLoginTimes($ip);
 		$params['isValid'] = $valid;
 
 		if(empty($_POST)){/* Open The Page At First !*/
-			$params['timesLogin'] = $timesLogin;
+			$params['timesLogin'] = $times_login;
 			$this->display('login.tpl', $params);
 			return;
 		}
@@ -25,7 +19,7 @@ class Default_Page extends Controller{
 
 		!$valid && exit('6');/*No validate IP!*/
 
-		if($timesLogin > 3){/* Validate The verifyed code!*/
+		if($times_login> 3){/* Validate The verifyed code!*/
 			empty($_POST['vcode']) && exit('7');
 			$_POST['vcode'] !== $_SESSION['vcode'] && exit('7');
 		}
@@ -35,13 +29,13 @@ class Default_Page extends Controller{
 		(empty($_POST['u']) || empty($_POST['p'])) && exit('2');
 		$username = $_POST['u'];
 		$password = md5($_POST['p']);
-		$userInfo = $user->login($username, $password);
-		if(!$userInfo){
+		$user_info = $user->login($username, $password);
+		if(!$user_info){
 			exit('2');
 		}
 
-		$user->clearClientIp($ip);
-		$_SESSION['userId'] = $userInfo->id;
+		$user->clearClientIP($ip);
+		$_SESSION['userId'] = $user_info->id;
 		exit('1');
 	}
 

bae68f8b0011b43b5f41ea92a641badd30a76ccc
baochuanzhou@gmail.com <baochuanzhou@gmail.com@31dcd45e-55b7-f6bc-f86d-74df720b1674>
2012-09-10 13:39:29 +0000

    添加工具类，避免使用全局函数

---
+++ trunk/app/Utility/class/Client.class.php
@@ -0,0 +1,11 @@
+<?php
+class Utility_Client{
+	public static function getClientIP(){
+		if(getenv('HTTP_CLIENT_IP')) return getenv('HTTP_CLIENT_IP');
+		if(getenv('HTTP_X_FORWARDED_FOR')) return getenv('HTTP_X_FORWARDED_FOR');
+		if(getenv('REMOTE_ADDR')) return getenv('REMOTE_ADDR');
+		if(isset($_SERVER['REMOTE_ADDR'])) return $_SERVER['REMOTE_ADDR'];
+
+		return false;
+	}
+}

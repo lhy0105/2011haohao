98d3b3b47ab0862fc8a88ed29f6468cec5db4021
baochuanzhou <BaochuanZhou@gmail.com>
2012-09-10 08:26:30 +0000

    兼容php版本

--- app/Default/class/Admin.class.php
+++ app/Default/class/Admin.class.php
@@ -8,7 +8,7 @@ class Default_Admin extends Controller{
 	public function page(){
 		$params = array();
 		$params['clientIP'] = get_client_ip();
-		$params['user'] = (new Default_Model_User())->getUserById();
+		$params['user'] = Default_Model_User::getInstance()->getUserById();
 		$this->display('page.tpl', $params);
 	}
 
